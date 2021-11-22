d40308e93b7978ed2b7c3bdd4c85e3175826e4b6
Ugo Stephant <ugo@poool.fr>
2021-11-22 21:56:55 +0100

    feat: better maps

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,6 @@
 export { default as farm } from './tileset-farm.png';
 export { default as interiorBasic } from './tileset-interior-basic.png';
 export { default as village } from './tileset-village.png';
+export { default as generic } from './tileset-generic.png';
+export { default as livingRoom } from './tileset-living-room.png';
+export { default as kitchen } from './tileset-kitchen.png';
